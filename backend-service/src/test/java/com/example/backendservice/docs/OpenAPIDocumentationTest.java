e782696cb2c236511fa03fe89eec8a85d75039cc
smitchell <stevecmitchell@gmail.com>
2021-05-07 10:40:54 -0400

    Simple api

--- backend-service/src/test/java/com/example/backendservice/docs/OpenAPIDocumentationTest.java
+++ backend-service/src/test/java/com/example/backendservice/docs/OpenAPIDocumentationTest.java
@@ -37,7 +37,7 @@ public class OpenAPIDocumentationTest {
 
     @Test
     public void writeSwaggerJson() throws Exception {
-        String outputDir = System.getProperty("openapi.outputDir", "src/main/resources/static/openapi");
+        String outputDir = System.getProperty("openapi.outputDir", "src/main/resources/static/docs");
         String json = mockMvc
                 .perform(
                         MockMvcRequestBuilders.get("/v3/api-docs")
